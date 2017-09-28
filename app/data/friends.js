f0166e66797b52ac5965068c48bed88c8f4f157b
Kristine Singleton <kmsing11@gmail.com>
2017-09-27 22:15:01 -0500

    adding files into repo

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [ {
+	"name" : "Olivia Pope",
+	"photo" : "https://blackgirlnerds.com/wp-content/uploads/2017/01/olivia-pope-episode-20-main.jpeg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Huck",
+	"photo" : "https://images-production.global.ssl.fastly.net/uploads/posts/image/128999/huck-dead-scandal.jpg?auto=compress&crop=faces,top&fit=crop&h=421&q=55&w=750",
+	"scores" : [1,5,3,3,3,3,3,3,3,4]
+}, {
+	"name" : "Jake",
+	"photo" : "https://radio.foxnews.com/wp-content/uploads/2014/11/Scott-Foley.jpg",
+	"scores" : [1,1,2,4,1,3,2,1,1,2]
+}, {
+	"name" : "Mellie",
+	"photo" : "https://peopledotcom.files.wordpress.com/2016/08/bellamy-young-1024-1.jpg?w=1024",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}];
+
+module.exports = friends;
+
+
+
